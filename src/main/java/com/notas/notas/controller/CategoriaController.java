4df18e7d21cd487dc6e6381e375a2cc0fcb8cb64
eqmoura01 <victorlvmora@gmail.com>
2024-07-31 21:57:10 -0300

    fix: corrige a implementacao do controller de categoria

--- src/main/java/com/notas/notas/controller/CategoriaController.java
+++ src/main/java/com/notas/notas/controller/CategoriaController.java
@@ -6,6 +6,11 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,26 +26,31 @@ public class CategoriaController {
     @Autowired
     private CategoriaService categoriaService;
 
+    @PostMapping
     public ResponseEntity<CategoriaDTO> save(@RequestBody CategoriaDTO categoriaDTO) {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(CategoriaMapper.toCategoriaDTO(categoriaService.save(CategoriaMapper.toCategoria(categoriaDTO))));
     }
 
+    @GetMapping
     public ResponseEntity<List<CategoriaDTO>> findAll() {
         return ResponseEntity.ok(categoriaService.findAll().stream().map(CategoriaMapper::toCategoriaDTO).collect(Collectors.toList()));
     }
 
-    public ResponseEntity<CategoriaDTO> findById(Long id) {
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoriaDTO> findById(@PathVariable Long id) {
         return ResponseEntity.ok(CategoriaMapper.toCategoriaDTO(categoriaService.findById(id)));
     }
 
+    @PutMapping
     public ResponseEntity<CategoriaDTO> update(@RequestBody CategoriaDTO categoriaDTO) {
 
         return ResponseEntity.ok(CategoriaMapper.toCategoriaDTO(categoriaService.update(CategoriaMapper.toCategoria(categoriaDTO))));
     }
 
-    public ResponseEntity<Void> delete(Long id) {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
         categoriaService.delete(id);
         return ResponseEntity.noContent().build();
     }
